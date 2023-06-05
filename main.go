4fd90c803a64d8061e8949b5c51afe6a65ecbee4
Munyao Kelvin <biggie@users-MacBook-Pro.local>
2023-06-05 20:49:11 +0300

    created MongoDB connect function

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"time"
+	"log"
+	"os"
+    
+	"github.com/urfave/cli/v2"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var collection *mongo.Collection
+var ctx = context.TODO()
+
+func init() {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client,err := mongo.Connect(ctx,clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx,nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	collection = client.Database("tasker").Collection("tasks")
+}
+
+func main() {
+	app := &cli.App{
+		Name: "tasker",
+		Usage: "A simple todo list manager",
+		Commands: []*cli.Command{},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type Task struct{
+	ID primitive.ObjectID `bson:"_id"`
+	CreatedAt time.Time `bson:"created_at"`
+	UpdatedAt time.Time `bson:"updated_at"`
+	Text string `bson:"text"`
+	Completed bool `bson:"completed"`
+
+}
